43be6a1e9990f7a4712553186385a8f919c5b203
a-akhmetshina <148335296+a-akhmetshina@users.noreply.github.com>
2023-11-28 18:16:07 +0500

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -1,30 +1,11 @@
+import requests
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores.qdrant import Qdrant
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+from typing import List
 import streamlit as st
-from openai import OpenAI
-
-
-client = OpenAI(
-  api_key=os.environ['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'],  # this is also the default, it can be omitted
-)
-
-st.sidebar.header("Chatbot Settings")
-
-st.title("💬 Chatbot")
-
-# if "messages" not in st.session_state:
-st.session_state["text"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state["text"]:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+import tiktoken
+from streamlit_chat import message
